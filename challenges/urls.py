19b06e030ec606fca30e3ba8836a001f523a6b66
lancegoyke <lancegoyke@gmail.com>
2020-03-28 09:50:02 -0700

    add search

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ChallengeListView, ChallengeDetailView
+from .views import ChallengeListView, ChallengeDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', ChallengeListView.as_view(), name="challenge_list"),
     path('<str:slug>', ChallengeDetailView.as_view(), name="challenge_detail"),
+    path('search/', SearchResultsListView.as_view(), name="search_results"),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 from .models import Challenge
 
@@ -16,3 +17,16 @@ class ChallengeDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'challenge'
     template_name = 'challenges/challenge_detail.html'
     login_url = 'account_login'
+
+
+class SearchResultsListView(ListView):
+    model = Challenge
+    context_object_name = 'challenges'
+    template_name = 'challenges/search_results.html'
+
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Challenge.objects.filter(
+            Q(name__icontains=query) | Q(name__icontains=query)
+        )
