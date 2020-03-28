2b38919b27f7ad1c8e9e106dca64b7558e917f93
lancegoyke <lancegoyke@gmail.com>
2020-03-28 07:42:16 -0700

    add login tests for challenge views

--- challenges/tests.py
+++ challenges/tests.py
@@ -31,14 +31,30 @@ class ChallengeTests(TestCase):
         self.assertEqual(f'{self.challenge.description}', 'This is a hard workout')
 
 
-    def test_challenge_list_view(self):
+    def test_challenge_list_view_for_logged_in_user(self):
+        self.client.login(email='recorduser@email.com', password='testpass123')
         response = self.client.get(reverse('challenge_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test challenge')
         self.assertTemplateUsed(response, 'challenges/challenge_list.html')
 
+
+    def test_challenge_list_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('challenge_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            '%s?next=/challenges/' % (reverse('account_login'))
+        )
+        response = self.client.get(
+            '%s?next=/challenges/' % (reverse('account_login'))
+        )
+        self.assertContains(response, 'Log In')
+
     
-    def test_challenge_detail_view(self):
+    def test_challenge_detail_view_for_logged_in_user(self):
+        self.client.login(email='recorduser@email.com', password='testpass123')
         response = self.client.get(self.challenge.get_absolute_url())
         no_response = self.client.get('/challenges/12345/')
         self.assertEqual(response.status_code, 200)
@@ -47,3 +63,11 @@ class ChallengeTests(TestCase):
         self.assertContains(response, 'recorduser')
         self.assertContains(response, '4:26:44')
         self.assertTemplateUsed(response, 'challenges/challenge_detail.html')
+
+
+    def test_challenge_detail_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(self.challenge.get_absolute_url())
+        no_response = self.client.get('/challenges/12345/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(no_response.status_code, 404)
