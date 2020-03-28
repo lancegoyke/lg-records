9837cc061846b1c32dc9332f1054829b182256ff
lancegoyke <lancegoyke@gmail.com>
2020-03-28 07:30:04 -0700

    add LoginRequiredMixin to challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,18 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Challenge
 
 # Create your views here.
-class ChallengeListView(ListView):
+class ChallengeListView(LoginRequiredMixin, ListView):
     model = Challenge
     context_object_name = 'challenges'
     template_name = 'challenges/challenge_list.html'
+    login_url = 'account_login'
 
 
-class ChallengeDetailView(DetailView):
+class ChallengeDetailView(LoginRequiredMixin, DetailView):
     model = Challenge
     context_object_name = 'challenge'
     template_name = 'challenges/challenge_detail.html'
+    login_url = 'account_login'
