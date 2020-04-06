efad6e299a75c2ac3818c714ac75ec8734e56fbf
lancegoyke <lancegoyke@gmail.com>
2020-04-06 12:53:35 -0700

    add Privacy Policy

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, PrivacyPageView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
+    path('privacy/', PrivacyPageView.as_view(), name='privacy'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,7 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class PrivacyPageView(TemplateView):
+    template_name = 'privacy.html'
