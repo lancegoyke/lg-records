b7f3870d01759f8f6c87af8de53f4576cb3ac9f2
lancegoyke <lancegoyke@gmail.com>
2020-04-06 09:48:17 -0700

    debug3

--- records_project/settings.py
+++ records_project/settings.py
@@ -151,7 +151,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email
