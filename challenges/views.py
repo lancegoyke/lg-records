abc1ab85e2d3ba51dab380eda412648b69a07f41
lancegoyke <lancegoyke@gmail.com>
2020-12-05 16:19:57 -0800

    linting

--- challenges/views.py
+++ challenges/views.py
@@ -20,16 +20,20 @@ from .models import Challenge, Record
 from .forms import ChallengeCreateForm, RecordCreateForm
 
 # Create your views here.
+
+
 @login_required()
 def challenge_filtered_list(request, slug=None):
     context = {}
     context['tag_list'] = Tag.objects.all()
     if slug is not None:
         # use the tag in the URL to filter challenges
-        context['filter'] = ChallengeFilter(request.GET, queryset=Challenge.objects.filter(tags__slug__in=[slug]).order_by('-date_created'))
+        context['filter'] = ChallengeFilter(request.GET, queryset=Challenge.objects.filter(
+            tags__slug__in=[slug]).order_by('-date_created'))
     else:
         # use all challenges
-        context['filter'] = ChallengeFilter(request.GET, queryset=Challenge.objects.all().order_by('-date_created'))
+        context['filter'] = ChallengeFilter(
+            request.GET, queryset=Challenge.objects.all().order_by('-date_created'))
     return render(request, 'challenges/challenge_filtered_list.html', context)
 
 
@@ -41,7 +45,8 @@ class ChallengeCreateView(PermissionRequiredMixin, CreateView):
     raise_exception = False
 
     def handle_no_permission(self):
-        messages.error(self.request, "You don't have permission to create a challenge.")
+        messages.error(
+            self.request, "You don't have permission to create a challenge.")
         return super(ChallengeCreateView, self).handle_no_permission()
 
 
@@ -49,13 +54,14 @@ class ChallengeUpdateView(PermissionRequiredMixin, UpdateView):
     model = Challenge
     form_class = ChallengeCreateForm
     template_name = "challenges/challenge_update.html"
-    fields = ['name', 'description', 'tags',]
+    # fields = ['name', 'description', 'tags', ]
     permission_required = ('challenges.can_edit',)
     raise_exception = False
     login_url = 'account_login'
 
     def handle_no_permission(self):
-        messages.error(self.request, "You don't have permission to update a challenge.")
+        messages.error(
+            self.request, "You don't have permission to update a challenge.")
         return super(ChallengeUpdateView, self).handle_no_permission()
 
 
@@ -66,20 +72,23 @@ class ChallengeDisplay(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['record_create_form'] = RecordCreateForm()
-        context['filter'] = RecordFilter(self.request.GET, queryset=self.get_object().records.order_by('-date_recorded'))
+        context['filter'] = RecordFilter(
+            self.request.GET, queryset=self.get_object().records.order_by('-date_recorded'))
         challenge_records = self.get_object().records.all()
         if challenge_records.exists():
-            context['top_score'] = challenge_records.order_by('time_score').first().time_score
+            context['top_score'] = challenge_records.order_by(
+                'time_score').first().time_score
             user_records = challenge_records.filter(user=self.request.user)
             if user_records.exists():
-                context['user_pr'] = user_records.order_by('time_score').first().time_score
+                context['user_pr'] = user_records.order_by(
+                    'time_score').first().time_score
         return context
 
 
 class RecordCreate(SingleObjectMixin, FormView):
     template_name = 'challenges/challenge_detail.html'
-    form_class = RecordCreateForm # FormView
-    model = Challenge # SingleObjectMixin
+    form_class = RecordCreateForm  # FormView
+    model = Challenge  # SingleObjectMixin
 
     def post(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
